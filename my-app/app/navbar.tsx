76ba16fd202f0f483f6edf067503275a945dc28b
Awais Webdeveloper <awaiswebdeveloper557@gmail.com>
2025-09-14 08:46:12 -0700

    Add Sign-up Page

--- my-app/app/navbar.tsx
+++ my-app/app/navbar.tsx
@@ -19,10 +19,10 @@ export default function Nav(){
           <ul className="flex gap-8 font-semibold">
           <a href=""><li>Home</li></a>
           <a href=""><li>About</li></a>
-          <Link href="/sinup">Contact Us</Link>
+          <Link href="">Todo List</Link>
           </ul>
         </div>
-        <Link href="/sinup"> <div ><button className="bg-emerald-600 text-white border h-12 w-30 rounded-3xl shadow-xl cursor-pointer font-semibold">Sign Up</button></div></Link>
+        <Link href="/signup"> <div ><button className="bg-emerald-600 text-white border h-12 w-30 rounded-3xl shadow-xl cursor-pointer font-semibold">Sign Up</button></div></Link>
       </section>
         
         
---
+++ my-app/app/signup/page.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+export default function SignUp(){
+    return(
+        <>
+        <h1>Sign Up</h1>
+        
+        </>
+    )
+}
